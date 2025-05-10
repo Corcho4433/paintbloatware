00c06575977f5fb611707b42c92b87e041471a58
Tu Nombre <you@example.com>
2025-05-10 12:11:03 -0300

    login skibidi

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import jacko from './assets/foo.jpg'
+import Basic from './login'
 import './App.css'
 import { Button } from './boton'
 
@@ -33,6 +34,8 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <div><Basic></Basic></div>
+      
     </>
   )
 }
