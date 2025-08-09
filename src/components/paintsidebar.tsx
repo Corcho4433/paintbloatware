17e1ae258776df73c8d323a2c59c08830b0f7468
gazoil2 <benicio920179@gmail.com>
2025-08-09 11:48:07 -0300

    lolo

--- src/components/paintsidebar.tsx
+++ src/components/paintsidebar.tsx
@@ -4,6 +4,7 @@ import {
   SidebarItemGroup,
 } from "flowbite-react";
 import { Link } from "react-router-dom";
+import { useAuthStore } from "../store/useAuthStore";
 import {
   LogInIcon,
   House,
@@ -12,40 +13,46 @@ import {
 } from "lucide-react";    
 
 const PaintSidebar = () => {
-    
+    const user = useAuthStore((state) => state.user);
+    const logout = useAuthStore((state) => state.logout); // Agregar esta línea
+
     return (
-    <Sidebar className="absolute left-0 h-full rounded-2xl">
-        <div className="flex justify-center"> <Bomb size={100}></Bomb></div>
-
-        <SidebarItemGroup>
-
-            <SidebarItem>
-                <Link to={"/home"} className="flex flex-row">
-                <House className="mr-4"></House>
-                Home
-                </Link>
-            </SidebarItem>
-            <SidebarItem>
-                <Link to={"/fastdraws"} className="flex flex-row">
-                <Bomb className="mr-4"></Bomb>
-                Fast Draws
-                </Link>
-            </SidebarItem>
-            <SidebarItem>
-                <Link to={"/draw"} className="flex flex-row">
-                <PencilIcon className="mr-4"></PencilIcon>
-                Draw
-                </Link>
-            </SidebarItem>
-            <SidebarItem>
-                <Link to={"/login"} className="flex flex-row">
-                <LogInIcon className="mr-4"></LogInIcon>
-                Register
-                </Link>
-            </SidebarItem>
-            
-        </SidebarItemGroup>
-      </Sidebar>
+        <Sidebar className="absolute left-0 h-full rounded-2xl">
+            <div className="flex justify-center"> <Bomb size={100}></Bomb></div>
+            <SidebarItemGroup>
+                <SidebarItem>
+                    <Link to={"/home"} className="flex flex-row">
+                        <House className="mr-4"></House>
+                        Home
+                    </Link>
+                </SidebarItem>
+                <SidebarItem>
+                    <Link to={"/fastdraws"} className="flex flex-row">
+                        <Bomb className="mr-4"></Bomb>
+                        Fast Draws
+                    </Link>
+                </SidebarItem>
+                <SidebarItem>
+                    <Link to={"/draw"} className="flex flex-row">
+                        <PencilIcon className="mr-4"></PencilIcon>
+                        Draw
+                    </Link>
+                </SidebarItem>
+                <SidebarItem>
+                    {user ? (
+                        <Link onClick={logout} to={"/login"} className="flex flex-row">
+                            <LogInIcon className="mr-4"></LogInIcon>
+                            Logout
+                        </Link>
+                    ) : (
+                        <Link to={"/login"} className="flex flex-row">
+                            <LogInIcon className="mr-4"></LogInIcon>
+                            Login
+                        </Link>
+                    )}
+                </SidebarItem>
+            </SidebarItemGroup>
+        </Sidebar>
     );
 }
 
--- src/store/useAuthStore.ts
+++ src/store/useAuthStore.ts
@@ -9,10 +9,12 @@ export interface User {
 interface AuthStore {
     user: User | null;
     setUser: (user: User) => void;
+    logout: () => void;
 }
 
 export const useAuthStore = create<AuthStore>((set) => ({
     user: null,
     setUser: (user) => set({ user }),
     logout: () => set({ user: null }),
+    hola: () => console.log("hola"),
 }));
