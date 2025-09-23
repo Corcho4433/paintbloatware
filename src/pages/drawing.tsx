3cdf90fe93232e86b7cb1b8f1da80d3dccd5be43
Corcho4433 <lisandro.julian07@gmail.com>
2025-09-23 15:54:45 -0300

    moved Frame type and interfaces

--- src/pages/drawing.tsx
+++ src/pages/drawing.tsx
@@ -1,25 +1,19 @@
 import { useEffect, useRef, useState } from 'react';
 import PaintSidebar from '../components/paintsidebar';
+import { processorRequest, processorResponse, processorServerResponse, processorAction, Frame } from '../types/draw';
 const GRID_SIZE = 32;
 const PIXEL_SIZE = 8;
 const INITIAL_FRAME = 1;
 const FRAME_RATE = 60;
 
-
-const MS_TIME = (1/FRAME_RATE) * 10000;
-
-type Frame = {
-    frame_id: number;
-    frame_data: number[][];
-}
-
+const MS_TIME = (1 / FRAME_RATE) * 10000;
 
 const Drawing = () => {
     const fullFrameAnimation: Map<number, Frame> = new Map();
     let currentFrame = INITIAL_FRAME;
     let currentInterval: number = 0;
     let isRunning = true;
-    
+
     const canvasRef = useRef<HTMLCanvasElement>(null);
     const [ws, setWs] = useState<WebSocket | null>(null);
     const [source, setSource] = useState("");
@@ -37,9 +31,10 @@ const Drawing = () => {
         };
 
         socket.onmessage = (event) => {
-            const newData = JSON.parse(event.data);
-
-            saveFrame(newData);
+            const newData = JSON.parse(event.data) as processorResponse;
+            if (newData.action === processorServerResponse.FrameData) {
+                saveFrame(newData.data.frame);
+            }
         };
 
         setWs(socket);
@@ -51,7 +46,7 @@ const Drawing = () => {
 
 
     const drawFrameAnimation = () => {
-        if (fullFrameAnimation.size <= 0) return; 
+        if (fullFrameAnimation.size <= 0) return;
         if (!isRunning) return;
 
         console.log("Is running?", isRunning);
@@ -125,7 +120,8 @@ const Drawing = () => {
 
     const handleRun = () => {
         if (ws && ws.readyState === WebSocket.OPEN) {
-            ws.send(source);
+            const packet = { action: processorAction.ProcessSourceCode, data: { source: source } } as processorRequest;
+            ws.send(JSON.stringify(packet));
         } else {
             console.warn("WebSocket no está conectado.");
         }
@@ -133,39 +129,39 @@ const Drawing = () => {
 
     return (
         <div className="flex">
-        
+
             <PaintSidebar />
             <div className="flex-1 min-h-screen bg-gray-900">
-            <div className='flex flex-col items-center'>
+                <div className='flex flex-col items-center'>
 
-                 <canvas
-                id="gridCanvas"
-                ref={canvasRef}
-                width={GRID_SIZE * PIXEL_SIZE}
-                height={GRID_SIZE * PIXEL_SIZE}
-                className='border-2 border-gray-700 rounded-lg mb-4 bg-gradient-to-br from-gray-900 to-black w-[512px] h-[512px]'
-            />
+                    <canvas
+                        id="gridCanvas"
+                        ref={canvasRef}
+                        width={GRID_SIZE * PIXEL_SIZE}
+                        height={GRID_SIZE * PIXEL_SIZE}
+                        className='border-2 border-gray-700 rounded-lg mb-4 bg-gradient-to-br from-gray-900 to-black w-[512px] h-[512px]'
+                    />
 
-            <textarea
-                id="source"
-                value={source}
-                onChange={(e) => setSource(e.target.value)}
-                className='bg-gray-800 rounded-lg p-2 mb-4'
-                style={{
-                    width: '500px',
-                    height: '200px',
-                    display: 'block',
-                    marginBottom: '1rem',
-                }}
-            />
+                    <textarea
+                        id="source"
+                        value={source}
+                        onChange={(e) => setSource(e.target.value)}
+                        className='bg-gray-800 rounded-lg p-2 mb-4'
+                        style={{
+                            width: '500px',
+                            height: '200px',
+                            display: 'block',
+                            marginBottom: '1rem',
+                        }}
+                    />
 
-                <button id="run" onClick={handleRun}>
-                    Run code
-                </button>
+                    <button id="run" onClick={handleRun}>
+                        Run code
+                    </button>
 
-                <button id="step" onClick={nextFrame}>
-                    Step (+1)
-                </button>
+                    <button id="step" onClick={nextFrame}>
+                        Step (+1)
+                    </button>
                 </div>
             </div>
         </div>
